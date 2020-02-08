f596722b0e80ce9c97b4167fe77fc1d297432d6d
Christian Perez <perezchristian1012@gmail.com>
2020-02-08 15:53:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const append = ["Milo", "Otis", "Garfield"]
 append.push("Odie")
 
@@ -9,3 +10,6 @@ removeLast.pop(2)
 
 const removeFirst = ["Milo", "Otis", "Garfield"]
 removeFirst.shift(0)
+=======
+function () append
+>>>>>>> faba7c6f9b96956574acd343ef2908c6f95b5f01
